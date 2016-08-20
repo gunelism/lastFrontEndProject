faf816e876e428ee8c7a2c6823e13438ffc18874
Günel İsmayılova <Gnel smaylova>
2016-08-20 16:22:19 +0400

    update

--- js/javas.js
+++ js/javas.js
@@ -1,26 +1,27 @@
 
 jQuery(document).ready(function($) {
-	// $(this).scrollTop(0);
 	$x=true;
 	$('#top1').fadeIn(1000);
 	$(window).scroll(function() {
+		// scroll ile animation-lar ishlemesi
   var scrollTop = $(window).scrollTop();
-  // var bounce = $('#trimg')
  	if(scrollTop>300){
- 		$('.yuxar').css('transform','translate(0px)'),
- 		$('.ashag').css('transform','translate(0px)')
+ 		$('.yuxar').css('transform','translateY(0px)'),
+ 		$('.ashag').css('transform','translateY(0px)')
  	}
  	if (scrollTop>600 && $x==true) {
 
- 		$('#trimg').css("transform","translateX(100px)"),
- 		$('#rightcome').css("transform","translateX(0px)")
+ 		$('#trimg').css("transform","translateX(40px)"),
+ 		$('#rightcome').css("transform","translateX(-40px)")
 
  
-	for (var i = 0; i < 2; i++) {
-		$('#trimg').animate({marginLeft:'+=100px'},"fast")
-		$('#trimg').animate({marginLeft:'-=100px'},'fast')
+	// for (var i = 0; i < 1; i++) {
+		$('#trimg').animate({marginLeft:'+=50px'},"fast")
+		$('#trimg').animate({marginLeft:'-=50px'},'fast')
+		$('#rightcome').animate({marginLeft:'-=50px'},"fast")
+		$('#rightcome').animate({marginLeft:'+=50px'},'fast')
 		$x=false;
-	}
+	// }
 
  	}
  	if (scrollTop>1100) {
@@ -31,6 +32,9 @@ jQuery(document).ready(function($) {
  		$('#trimg3').css("transform","translateX(0px)"),
  		$('#rightcome3').css("transform","translateX(0px)")
  	}
+ 	// burda bitir scroll animation
+
+ 	//scroll ile navbarin gorunmesi
  	 if (scrollTop>600) {
 		 	$('nav').removeClass('head');
 		 	$('nav').addClass('head2');
@@ -38,28 +42,112 @@ jQuery(document).ready(function($) {
 		 	$('nav').removeClass('head2');
 		 	$('nav').addClass('head');
 		 }
-
+	
  
 });
- 	$('#tab button').click(function(){
- 		$('#tab button').removeClass('active');
- 		$(this).addClass('active');
+	//navbara basarken active classlarin deyishmesi
+ 	$('li').on('click',function(){
+
+ 		$('li').removeClass('active1');
+ 		$(this).addClass('active1');
+
  	});
+
+//tab section hissede her 3 duymeye aid funksiya ve animatinlarin ishe 
+//dushmesi. burada birinci button ucun baslayir
  			
  	$('.btn1').click(function(event) {
+
+
  		$('.first').css('display','block');
  		$('.second').css('display','none');
  		$('.third').css('display','none');
+
+ 		 $('.fromTop').css('transform','translateY(-250px)');
+
+
+ 		setTimeout(function (argument) {
+ 			  $('#rightcome').css('transform','translateX(-50px)');
+ 			  $('#trimg').css('transform','translateX(50px)');
+ 		},1)
+
+ 			for (var i=0; i<1; i++) {
+ 				$('#rightcome').animate({marginLeft: '-=50px'}, 'fast')
+ 				$('#rightcome').animate({marginLeft: '+=50px'}, 'fast')
+ 				$('#trimg').animate({marginLeft: '+=50px'}, 'fast')
+ 				$('#trimg').animate({marginLeft: '-=50px'}, 'fast')
+ 				$x=false;
+ 			}
+ 	
+
+ 			$('#tabRightCome1').css('transform','translateX(800px)');
+ 		$('#tabLeftCome1').css('transform','translateX(-800px)');
+ 		$('#tabRightCome1').css({marginLeft: '0px'});
+ 		$('#tabLeftCome1').css({marginLeft: '0px'});
  	});
+
+//ikinci button bu hissede bashlayir
  	$('.btn2').click(function(event) {
+
+ 		$('#rightcome').css('transform','translateX(800px)');
+ 		$('#trimg').css('transform','translateX(-800px)');
+
+ 		$('#rightcome').css({marginLeft: '0px'});
+ 		$('#trimg').css({marginLeft: '0px'});
+
+
+ 		 $('.fromTop').css('transform','translateY(-250px)');
+
+
+ 		setTimeout(function (argument) {
+ 			  $('#tabRightCome1').css('transform','translateX(-40px)');
+ 			  $('#tabLeftCome1').css('transform','translateX(40px)');
+ 		},1)
+
+ 		// for (var i=0; i<1; i++) {
+ 		// 		$('#tabRightCome1').animate({marginLeft: '-=40px'}, 'fast')
+ 		// 		$('#tabRightCome1').animate({marginLeft: '+=40px'}, 'fast')
+ 		// 		$('#tabLeftCome1').animate({marginLeft: '+=40px'}, 'fast')
+ 		// 		$('#tabLeftCome1').animate({marginLeft: '-=40px'}, 'fast')
+ 		// 		$x=false;
+ 		// 	}
+
+
+
  		$('.first').css('display','none');
  		$('.second').css('display','block');
  		$('.third').css('display','none');
  	});
+ 	
+ //ucuncu button ise burada bashlayir
  	$('.btn3').click(function(event) {
  		$('.first').css('display','none');
  		$('.second').css('display','none');
  		$('.third').css('display','block');
+ 		
+	 		setTimeout(function (argument){
+
+	 		$('.fromTop').css({transform:'translateY(0px)'})
+ 		// $('#textshow').css('fontSize', '16px') ???????????????????
+
+	 		},1);
+
+
+
+ 		$('#rightcome').css('transform','translateX(800px)');
+ 		$('#trimg').css('transform','translateX(-800px)');
+ 		$('#rightcome').css({marginLeft: '0px'});
+ 		$('#trimg').css({marginLeft: '0px'});
+
+ 		$('#tabRightCome1').css('transform','translateX(800px)');
+ 		$('#tabLeftCome1').css('transform','translateX(-800px)');
+ 		$('#tabRightCome1').css({marginLeft: '0px'});
+ 		$('#tabLeftCome1').css({marginLeft: '0px'});
+
+ 	// ????	$('#textShow').show('slow');
+
+
+ 		
  	});
 
 });
